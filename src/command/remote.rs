cbc257e6822e812c92d30c21001cf83742970613
goushiking <3071786182@qq.com>
2026-01-17 22:27:53 +0800

    Update remote.rs
    Signed-off-by: goushiking <3071786182@qq.com>

--- src/command/remote.rs
+++ src/command/remote.rs
@@ -1,6 +1,8 @@
 // Manages remotes by listing, showing, adding, and updating URLs and associated fetch/push metadata.
-use clap::Subcommand;
-use crate::internal::config::Config;
+use {
+    clap::Subcommand,
+    crate::internal::config::Config,
+};
 
 #[derive(Subcommand, Debug)]
 pub enum RemoteCmds {
