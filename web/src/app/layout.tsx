180d584e27a98e7d403f46cb57f06e85bc9a8bbb
Quanyi Ma <eli@patch.sh>
2026-03-15 00:31:45 +0800

    fix(web): use local fonts and change default port to 17654
    - Switch from next/font/google to next/font/local with bundled WOFF2
      font files to avoid build failures when Google Fonts is unreachable
    - Remove unused geist npm dependency
    - Change default web server port from 3000 to 17654 to avoid conflicts
      with Next.js dev server
    
    Signed-off-by: Eli <eli@patch.sh>
    Signed-off-by: Quanyi Ma <eli@patch.sh>

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -3,13 +3,13 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "./fonts/GeistVF.woff2",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "./fonts/GeistMonoVF.woff2",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
